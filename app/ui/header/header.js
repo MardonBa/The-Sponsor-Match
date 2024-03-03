39cc70601b0322a22bc25d4c27c074382245e43e
Bayan Mardon <mardon.junior@gmail.com>
2024-03-03 12:45:48 -0500

    fix spacing issues on navigation links

--- app/ui/header/header.js
+++ app/ui/header/header.js
@@ -6,8 +6,10 @@ export default function LandingHeader() {
     return (
         <div className={styles.landingheader}>
             <p className={styles.logo}>logo</p> {/* Will eventually be an <Image /> with the logo */}
-            <HeaderCard text="features" gridStyle={styles.features} hoverstyle={styles.hoverunderline} includeArrow={true} />
-            <HeaderCard text="pricing" gridStyle={styles.pricing} hoverstyle={styles.hoverunderline} />
+            <div className={styles.navcontainer} >
+                <HeaderCard text="features" gridStyle={styles.features} hoverstyle={styles.hoverunderline} includeArrow={true} />
+                <HeaderCard text="pricing" gridStyle={styles.pricing} hoverstyle={styles.hoverunderline} />
+            </div>
             <div className={styles.accountcontainer} >
                 <HeaderCard text="sign up" gridStyle={styles.signup} />
                 <HeaderCard text="sign in" gridStyle={styles.signin} />
