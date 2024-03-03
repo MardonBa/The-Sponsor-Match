79a44929b47e0225f05b3f3ce839a10c689e1780
Bayan Mardon <mardon.junior@gmail.com>
2024-03-03 12:35:52 -0500

    fix spacing issues for sign up / sign in

--- app/page.js
+++ app/page.js
@@ -7,13 +7,16 @@ export default function LandingPage() {
     <div className={styles.maincontainer} >
         <LandingHeader />
         <h1 className={`${styles.colorgradient} ${styles.h1}`} ><b>The Sponsor Match</b></h1>
-        <section className={styles.mainpage}>
+        <section className={styles.contentone}>
             <h3 className={`${styles.h3} ${styles.secondaryunderline}`} >Start a relationship to promote your product or make money off of your content</h3>
             <p className={styles.p} >With our AI-powered matching tool, finding partners has never been easier. 
                 Just enter your criteria, and our algorithm will find the best creators to promote your product.
                 If you're a creator, the process is just as simple. 
                 Our platform connects you with potential sponsors based on your preferences in order to give the best possible experience in monetizing yourself and your content.
                 </p>
+        </section>
+        <section className={styles.contenttwo} >
+            <h3 className={styles.h3} >hello world</h3>
         </section>
     </div>
   );
