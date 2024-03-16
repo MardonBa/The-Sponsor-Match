86d6e4e2a94b9d72da68b8fffa0d55f8f2237f37
Bayan Mardon <mardon.junior@gmail.com>
2024-03-16 10:47:11 -0400

    finish styling and filling the creator/sponsor section

--- app/ui/comparisonCard/creators.js
+++ app/ui/comparisonCard/creators.js
@@ -5,7 +5,7 @@ export default function CreatorText() {
         <ul className={styles.list}>
             <li>Customize your profile based on your platform(s), content style, and sponsorship preferences</li>
             <li>Find sponsors that match your creative vision and style using our matching tool</li>
-            <li>Either make content for you to publish yourself, or for you to send over to the sponsor for them to publish</li>
+            <li>Either make content for you to publish yourself, or for you to send over to the sponsor for them to publish. Make all communication easy with our built-in chat</li>
             <li>Analyze which sponsorships appeal most to your viewers, and use that information to build long-term relationships with sponsors</li>
             <li>Handle paymnets with ease, with no need to use a third-party app to get paid. Once the content as been published and the term of the deal have been confirmed by both parties, the payment will go through automatically</li>
             <li>Have any worries? Users have access to our customer support, which will work with you to make sure that any issues you have are resolved</li>
--- app/ui/comparisonCard/sponsors.js
+++ app/ui/comparisonCard/sponsors.js
@@ -2,8 +2,13 @@ import styles from "./comparisonCard.module.css";
 
 export default function SponsorText() {
     return (
-        <div>
-            <h2 className={styles.h2} >Sponsors</h2>
-        </div>
+        <ul className={styles.list}>
+            <li>Select what platforms you want your products to appear on, what content types, and what genre of creators you want promoting your products</li>
+            <li>Get matched with like-minded creators that will effectively promote your product using our matching tool</li>
+            <li>Work with the creators on what you want the sponsored content to look like, and be in constant communication with creators using our in-house chat</li>
+            <li>Use our analytics tools to determine which creators work best for your brand, and maximize engagement</li>
+            <li>Handle paymnets with ease, with no need to use a third-party app to get paid. Once the content as been published and the term of the deal have been confirmed by both parties, the payment will go through automatically</li>
+            <li>Have any worries? Users have access to our customer support, which will work with you to make sure that any issues you have are resolved</li>
+        </ul>
     );
 }
