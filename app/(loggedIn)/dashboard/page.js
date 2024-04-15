6a27e8a3c3ca86a7ce69f7a39c3a177697ccd456
Bayan Mardon <mardon.junior@gmail.com>
2024-04-14 21:59:16 -0400

    begin work on user dashboard

--- app/(loggedIn)/dashboard/page.js
+++ app/(loggedIn)/dashboard/page.js
@@ -1,8 +1,20 @@
 import styles from './page.module.css';
+import { Sidebar } from '@/app/ui/loggedIn/dashboardComponents/sidebar';
 
 export default function Dashboard() {
     return (
         <div className={styles.dashboardcontainer} >
+            <Sidebar className={`${styles.sidebar} ${styles.navsidebar}`} />
+            <div className={styles.maincontentcontainer} >
+                <div className={styles.searchcontainer} >
+
+                </div>
+                <div className={styles.managementcontainer} >
+                {/* This container either has campaigns if the user is a sponsor or current/recent sponsorships for creators */}
+
+                </div>
+            </div>
+            <Sidebar className={`${styles.sidebar} ${styles.messagessidebar}`} />
         </div>
     );
 }
---
+++ app/ui/loggedIn/dashboardComponents/sidebar.js
@@ -0,0 +1,11 @@
+import styles from "./sidebar.module.css";
+
+export function Sidebar({ className }) {
+    // Should eventually track the state and have 2 styles for the sidebar: collapsed and full
+    // State should change whenever the button is pressed
+    return (
+        <div className={className} >
+            sidebar
+        </div>
+    );
+}
