1cfb5fe4053164bcfd811a2553924d74078783a7
mardon.junior <mardon.junior@gmail.com>
2024-05-31 04:38:39 +0000

    create initial styles for the search results section

--- app/ui/loggedIn/dashboardComponents/search/searchResults.js
+++ app/ui/loggedIn/dashboardComponents/search/searchResults.js
@@ -1,9 +1,15 @@
 import styles from './searchResults.module.css';
+import SearchResult from './searchResults/searchResult';
 
 export default function SearchResults() {
     return (
         <div className={styles.container} >
-            <p>Hello World</p>
+            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
         </div>
     );
 }
--- app/ui/loggedIn/dashboardComponents/search/searchResults/searchResult.js
+++ app/ui/loggedIn/dashboardComponents/search/searchResults/searchResult.js
@@ -1,10 +1,17 @@
 import styles from './searchResult.module.css';
 import ProfilePicPreview from '../../profilePicPreview/profilePicPreview';
 
-export default function SearchResult() {
+export default function SearchResult({ name, communitySize, niche, contentFrequency }) {
+    // Likely what will end up happening is instead of each item being passed in as a prop, the user's id will be passed in and a db query will be used to get all of the relevant information.
+    // This prop-based solution is only for layout purposes pre-db
     return (
-        <div>
-
+        <div className={styles.container} >
+            <ProfilePicPreview />
+            <p className={styles.name} >{name}</p>
+            <p className={styles.communitysize} >{communitySize}</p>
+            <p className={styles.niche} >{niche}</p>
+            <p className={styles.contentfrequency} >{contentFrequency}</p>
+            <div className={styles.socials} >icons {/* Shuold have an icon linking to their account on each platform */}</div>
         </div>
     );
 }
