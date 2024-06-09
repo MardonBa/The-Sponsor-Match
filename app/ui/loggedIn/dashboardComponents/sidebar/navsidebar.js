f54dc6464e44937dc6893808f57970bd0aa8e183
Bayan Mardon <mardon.junior@gmail.com>
2024-06-09 18:24:25 -0400

    begin adding icons to navigation sidebar

--- app/ui/loggedIn/dashboardComponents/sidebar/navsidebar.js
+++ app/ui/loggedIn/dashboardComponents/sidebar/navsidebar.js
@@ -2,6 +2,7 @@
 
 import styles from './navsidebar.module.css';
 import React from 'react';
+import Image from 'next/image';
 
 export default function NavSideBar() {
     /*
@@ -13,19 +14,22 @@ export default function NavSideBar() {
     Maybe introduce redux and redux toolkit for app-wide state management, since the other components need to update their size accordingly
     Try setting the width of the other elements to a percentage first, no need to over-engineer
     */
-    const [isCollapsed, setIsCollapsed] =  React.useState(false); // defaults to false because the initial state should be 
+    const [isCollapsed, setIsCollapsed] =  React.useState(false); // defaults to false because the initial state should be full width
 
     let button;
     let widthStyle;
     let logo;
+    let navigation;
     if (isCollapsed) {
         button = '>';
         widthStyle = styles.collapsed;
         logo = 'TSM';
+        navigation = NavIcons();
     } else {
         button = '<';
         widthStyle = styles.fullwidth;
         logo = 'The Sponsor Match';
+        navigation = NavLinksFullWidth();
     }
 
     function handleCollapseClick() {
@@ -38,6 +42,14 @@ export default function NavSideBar() {
                 <h1 className={`${styles.colorgradient} ${styles.logolarge}`} ><b>{logo}</b></h1>
                 <p className={styles.collapsebutton} onClick={handleCollapseClick} >{button}</p>
             </div>
+            {navigation}
+        </div>
+    );
+}
+
+function NavLinksFullWidth() {
+    return (
+        <>
             <p className={styles.item} >Dashboard</p>
             <p className={styles.item} >Search</p>
             <p className={styles.item} >Analytics</p>
@@ -52,6 +64,34 @@ export default function NavSideBar() {
             <p className={styles.item} >Advertise</p>
             <p className={styles.item} >Settings</p>
             <p className={styles.item} >Log Out</p>
-        </div>
+        </>
+    );
+}
+
+function NavIcons() {
+    return (
+        <>
+            <Image
+                src='/icons/icon.svg'
+                width={24}
+                height={24}
+                alt='dashboard icon'
+                className={styles.item}
+            />
+            <p className={styles.item} >Dashboard</p>
+            <p className={styles.item} >Search</p>
+            <p className={styles.item} >Analytics</p>
+            <p className={styles.item} >Payment History</p>
+            <p className={styles.item} >Legal Documents</p>
+            <p className={styles.item} >Partnerships</p>
+            <p className={styles.item} >Messages</p>
+            <div className={styles.divider} >
+
+            </div>
+            <p className={styles.item} >Contact</p>
+            <p className={styles.item} >Advertise</p>
+            <p className={styles.item} >Settings</p>
+            <p className={styles.item} >Log Out</p>
+        </>
     );
 }
