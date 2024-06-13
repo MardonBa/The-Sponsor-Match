a1707056f2ad68f5acbf4abaf94f77ff0ac26e32
Bayan Mardon <mardon.junior@gmail.com>
2024-06-13 09:09:45 -0400

    change landing page text

--- app/(notLoggedIn)/page.js
+++ app/(notLoggedIn)/page.js
@@ -10,7 +10,7 @@ export default function LandingPage() {
     <div className={styles.maincontainer} >
         <h1 className={`${styles.colorgradient} ${styles.h1}`} ><b>The Sponsor Match</b></h1>
         <section className={styles.contentone}>
-          <h3 className={`${styles.h3} ${styles.secondaryunderline}`} >Start a relationship to promote your product or make money off of your content</h3>
+          <h3 className={`${styles.h3} ${styles.secondaryunderline}`} >Monetize your content or raise awareness about your product with ease</h3>
           <p className={styles.p} >
             With our AI-powered matching tool, finding partners has never been easier. 
             Just enter your criteria, and our algorithm will find the best creators to promote your product.
