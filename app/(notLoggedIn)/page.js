0f7a60dfda43881a01ee2b8c1d52dafa58fd9ff3
Bayan Mardon <mardon.junior@gmail.com>
2024-06-15 08:31:26 -0400

    replace apostrophes with shorthand

--- app/(notLoggedIn)/page.js
+++ app/(notLoggedIn)/page.js
@@ -14,7 +14,7 @@ export default function LandingPage() {
           <p className={styles.p} >
             With our AI-powered matching tool, finding partners has never been easier. 
             Just enter your criteria, and our algorithm will find the best creators to promote your product.
-            If you're a creator, the process is just as simple. 
+            If you&apos;re a creator, the process is just as simple. 
             Our platform connects you with potential sponsors based on your preferences in order to give the best possible experience in monetizing yourself and your content.
           </p>
           <LinkToSignUp text={"Start Now"} color={'secondary'} />
@@ -22,7 +22,7 @@ export default function LandingPage() {
         <section className={styles.contenttwo} >
             <h3 className={`${styles.h3} ${styles.primaryunderline}`} >Use our data and analytics to maximize engagement and profits.</h3>
             <p className={styles.p} >
-              Whether you're a content creator or a sponsor, being able to track engagement and performace is a top priority.
+              Whether you&apos;re a content creator or a sponsor, being able to track engagement and performace is a top priority.
               For sponsors, see which campaigns are resulting in the most conversions.
               For creators, see which products are resonating most with your audience.
               These metrics allow for better matches to be made, allowing for an ultra-effective matching tool, and maximized profits for all.
