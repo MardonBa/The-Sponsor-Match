ced0329885593e5206a09f1ef6f2b39b9e08ba48
Bayan Mardon <mardon.junior@gmail.com>
2024-07-29 18:19:52 -0400

    change href to redirect to the right link for signing in an dup

--- app/ui/notLoggedIn/header/headerCard.js
+++ app/ui/notLoggedIn/header/headerCard.js
@@ -4,8 +4,9 @@ import Link from 'next/link';
 export default function HeaderCard({ text, gridStyle, hoverstyle=undefined }) {
     // hoverstyle used for all cards except the sign in and sign up
     // ArrowComponent is only there for the features dropdown, all others will have it as undefined
+    let href_text = text.replace(" ", "-")
     return (
-    <Link className={`${styles.card} ${gridStyle}`} href={`/${text}`} > {/* Setting the  href to the text makes it easy to use custom linking*/}
+    <Link className={`${styles.card} ${gridStyle}`} href={`/${href_text}`} > {/* Setting the  href to the text makes it easy to use custom linking*/}
         <p className={`${styles.text} ${hoverstyle}`}>{text}</p>
     </Link>
     );
