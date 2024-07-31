120e12730231e2ee9bbaa3ff770bc96502643e8f
Bayan Mardon <mardon.junior@gmail.com>
2024-07-30 23:06:24 -0400

    start work on sign-in page

--- app/(notLoggedIn)/sign-in/page.js
+++ app/(notLoggedIn)/sign-in/page.js
@@ -2,8 +2,8 @@ import styles from './page.module.css';
 
 export default function Page() {
     return (
-        <>
-        
-        </>
+        <div className={styles.container} >
+            
+        </div>
     );
 }
