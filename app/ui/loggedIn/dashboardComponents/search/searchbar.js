2fe7bf70fcbc05b5b34954a53fc877fe3e36aabf
Bayan Mardon <mardon.junior@gmail.com>
2024-07-31 10:22:12 -0400

    fix overflow on search bar to make it scrollable

--- app/ui/loggedIn/dashboardComponents/search/searchbar.js
+++ app/ui/loggedIn/dashboardComponents/search/searchbar.js
@@ -4,7 +4,7 @@ import DropdownContainer from './dropdowns/dropdownContainer';
 export default function SearchBar() {
     return (
         <div className={styles.container} >
-            <textarea className={styles.searchbar} >
+            <textarea className={styles.searchbar} rows="1" >
                 {/*
                 Probably need to set up redux to manage the state of the searchbar (what text has been entered)
                 Might not be necessary, it just depends on how the databse can be queried when the user presses search
