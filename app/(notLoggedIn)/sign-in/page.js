6e2d095d0efc215b42241fa77a21dbe715a5ca3f
Bayan Mardon <mardon.junior@gmail.com>
2024-07-31 10:19:59 -0400

    start working on sign in and log in pages

--- app/(notLoggedIn)/sign-in/page.js
+++ app/(notLoggedIn)/sign-in/page.js
@@ -3,7 +3,10 @@ import styles from './page.module.css';
 export default function Page() {
     return (
         <div className={styles.container} >
-            
+            <h2 className={styles.h2} >Log In to The Sponsor Match</h2>
+            <form>
+                <label></label>
+            </form>
         </div>
     );
 }
