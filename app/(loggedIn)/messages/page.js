8df30e65d2c339f97bc21b3cff052e56858a5e7e
Bayan Mardon <mardon.junior@gmail.com>
2024-08-14 19:12:33 -0400

    add a searchbar to the messages page

--- app/(loggedIn)/messages/page.js
+++ app/(loggedIn)/messages/page.js
@@ -1,20 +1,8 @@
-import styles from './page.module.css';
-import Sidebar from '@/app/ui/loggedIn/dashboardComponents/sidebar/sidebar';
-import MessagesSidebar from '@/app/ui/loggedIn/dashboardComponents/sidebar/messages/messageSidebar';
-import NavSideBar from '@/app/ui/loggedIn/dashboardComponents/sidebar/navsidebar';
 
 export default function MessagesPage() {
     return (
-        <div className={styles.pagecontainer} >
-            <Sidebar style={styles.navsidebar} >
-                <NavSideBar forceCollapse={true} />
-            </Sidebar>
-            <Sidebar style={styles.messagesidebar} >
-                <MessagesSidebar forceOpen={true} />
-            </Sidebar>
-            <div className={styles.messagescontainer} >
-
-            </div>
-        </div>
+        <>
+            {/* Messaages go in here */}
+        </>
     );
 }
