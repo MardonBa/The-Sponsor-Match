da9c164a5181dc4b0c7e5f045ce35297a2e5956d
Bayan Mardon <mardon.junior@gmail.com>
2024-09-29 14:20:48 -0400

    make default scroll of messages be the bottom

--- app/(loggedIn)/messages/page.js
+++ app/(loggedIn)/messages/page.js
@@ -1,15 +1,33 @@
+"use client"
+
 import styles from './page.module.css';
 import Message from "@/app/ui/loggedIn/messagesComponents/message/message";
 import SendButton from "@/app/ui/loggedIn/messagesComponents/sendButton/sendButton";
-
+import { useRef, useEffect, useState } from "react";
 
 export default function MessagesPage() {
 
-        
+    const messagesRef = useRef(null);
+    const messageTextRef = useRef(null);
+    const [messageText, setMessageText] = useState("");
+
+    useEffect(() => {
+        const messages = messagesRef.current;
+        console.log(messages);
+        if (messages) {
+            console.log("working");
+            messages.scrollTop = messages.scrollHeight;
+        }
+    }, [messageText]);
+
+    const onMessageTextChange = (e) => {
+        setMessageText(e.target.textContent);
+        console.log(`Message text: ${messageText}`);
+    }
 
     return (
         <div className={styles.messagescontainer} >
-            <div className={styles.messages} >
+            <div className={styles.messages} ref={messagesRef}  >
                 <Message fromUser={true} messageContent={'darkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbg'}/>
                 <Message fromUser={false} messageContent={'darkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbg'}/>
                 <Message fromUser={true} messageContent={'darkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbg'}/>
@@ -22,7 +40,7 @@ export default function MessagesPage() {
                 <Message fromUser={false} messageContent={'darkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbgdarkbg'}/>
             </div>
             <div className={styles.searchbarcontainer} >
-                <span className={styles.searchbar} role="textbox" contentEditable spellcheck="true"  />
+                <span className={styles.searchbar} role="textbox" contentEditable spellCheck="true" ref={messageTextRef} onInput={onMessageTextChange} />
                 <div className={styles.sendbutton} >
                     <SendButton />
                     {/* 
