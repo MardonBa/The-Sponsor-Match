108074fc095c33e580ad6a3dade517b01dfe097d
Bayan Mardon <mardon.junior@gmail.com>
2024-11-11 20:00:56 -0500

    fix sign in with google bug

--- app/ui/public/auth/action.js
+++ app/ui/public/auth/action.js
@@ -9,7 +9,7 @@ export async function loginWithGoogle(response) {
   const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-          redirectTo: 'localhost:3000/auth/callback'
+          redirectTo: 'http://localhost:3000/auth/callback'
       }
   });
 
--- app/ui/public/auth/authComponent.js
+++ app/ui/public/auth/authComponent.js
@@ -23,19 +23,21 @@ export default function AuthComponent({ buttonText, action }) {
 
   return (
     <div className={styles.container} >
-      <form className={styles.loginform} >
-        <input id="email" name="email" type="email" required className={styles.input} placeholder="Email" />
-        <input id="password" name="password" type="password" required className={styles.input} placeholder="Password" />
-        <button formAction={action} className={styles.button} >{buttonText}</button>
-      </form>
-      <div className={styles.divider} >
-        <div className={styles.line} ></div>
-        <span className={styles.text} >or</span>
-        <div className={styles.line}></div>
-      </div>
-      <GoogleButton clickAction={loginWithGoogle} buttonStyles={styles.googlebutton} />
-      <div className={styles.nav} >
-        <p>{navText} <Link href={link} className={styles.navlink} >{navButton}</Link></p>
+      <div className={styles.formcontainer} >
+        <form className={styles.loginform} >
+          <input id="email" name="email" type="email" required className={styles.input} placeholder="Email" />
+          <input id="password" name="password" type="password" required className={styles.input} placeholder="Password" />
+          <button formAction={action} className={styles.button} >{buttonText}</button>
+        </form>
+        <div className={styles.divider} >
+          <div className={styles.line} ></div>
+          <span className={styles.text} >or</span>
+          <div className={styles.line}></div>
+        </div>
+        <GoogleButton clickAction={loginWithGoogle} buttonStyles={styles.googlebutton} />
+        <div className={styles.nav} >
+          <p>{navText} <Link href={link} className={styles.navlink} >{navButton}</Link></p>
+        </div>
       </div>
     </div>
   );
