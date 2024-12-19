2ad009d733d3e12c5832fc6721de64797d4fe8df
Bayan Mardon <mardon.junior@gmail.com>
2024-12-19 17:48:45 -0500

    continue working

--- app/(auth)/update-password/action.js
+++ app/(auth)/update-password/action.js
@@ -1,12 +1,39 @@
 "use server";
 
-import { createClient } from "@/utils/supabase/server";
+//import { createClient } from "@/utils/supabase/server";
 import { validatePassword } from "@/app/lib/auth/validation";
 import { redirect } from "next/navigation";
+import { createClient } from "@supabase/supabase-js";
 
+/*
+- create an admin client
+- get the uid from the email
+https://stackoverflow.com/questions/68334303/supabase-how-to-query-users-by-email
+- update password, access the user through their uid
+*/
 
-export default async function resetPassword(newPassword) {
-  const supabase = await createClient();
+// get user id by email when they enter it
+// put it in a query string for the update-password redirect
+// pass the user id into this function
+// update the password based on the user id (maybe need to create a session, update, clear the session)
+
+export default async function resetPassword(email, newPassword) {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY
+  );
+  console.log(email);
+  const { data, error } = await supabase.rpc('get_user_id_by_email', { email }, { get: true });
+  console.log(data);
+
+  //const { data, error } = await supabase.auth.admin.getUserById(1)
+
+  if (error) {
+    // TODO error handling
+    // there shouldn't be an error because the email is already checked to exist earlier
+  }
+
+  /*
 
   const validPassword = validatePassword(newPassword);
   // for some reason the user is being authenticated when this gets called, that is wrong
@@ -22,4 +49,6 @@ export default async function resetPassword(newPassword) {
   } else {
     // TODO handle what happens. this should never really run though since the password is validated on the client side first
   }
+
+  */
 }
--- app/(public)/forgot-password/action.js
+++ app/(public)/forgot-password/action.js
@@ -1,16 +1,49 @@
 "use server";
 
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@supabase/supabase-js";
 
 export default async function handleRequestPasswordResetLink(email) {
-  const supabase = await createClient();
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY
+  );
 
-  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: 'http://localhost:3000/update-password',
-  })
-
+  const { data, error } = await supabase.rpc('get_user_id_by_email', { email: email }, { get: true });
+  console.log(email);
   if (error) {
     // handle error
-    console.log("err: " + error);
+    //console.log("err: " + error);
+    throw error;
+    return;
   }
+
+  fetch(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/rpc/get_user_id_by_email`, {
+    method: 'POST',
+    headers: {
+      apiKey: process.env.SUPABASE_SERVICE_ROLE_KEY,
+      Authorization: `Bearer ${process.env.SUPABASE_SERVICE_ROLE_KEY}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email: 'mardon.junior@gmail.com' }),
+  })
+    .then((res) => res.json())
+    .then(console.log)
+    .catch(console.error);
+  //console.log(data);
+  //const id = data[0].id; // Gets the user id returned from the function
+
+  // the data returned by this function doesn't need to be used
+  // the error doesn't need to be used either, because it would raise an error if the email doesn't
+  // exist, which is already checked for by the previous function and if-statement
+
+  // i think calling this function is what creates an authenticated session
+  // TODO: for some reason i keep getting an invalid email url. This needs fixing
+  // hopefully this should be fixed. the server is slow and is making testing if my code works a pain, but test this when i can
+  // TODO: from here i need to update the user's password in the update-password action, then figure out how to prevent an authenticated session from being created
+  await supabase.auth.resetPasswordForEmail(email, {
+    redirectTo: `/update-password?id=${id}`
+  })
+
+  // make sure user isn't authenticated after this action is called
+
 }
