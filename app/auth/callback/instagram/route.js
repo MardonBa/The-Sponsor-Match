6856ff1512b0ddb925e2e64f78f56d85f0690935
Bayan Mardon <mardon.junior@gmail.com>
2025-01-02 13:37:39 -0500

    add iniitial more code handling logic for ig oauth

--- app/auth/callback/instagram/route.js
+++ app/auth/callback/instagram/route.js
@@ -29,8 +29,36 @@ export async function GET(request) {
     const { accessToken, userId } = tokenData;
     
     // exchange the short lived token for a long lived one https://developers.facebook.com/docs/instagram-platform/instagram-api-with-instagram-login/business-login
+    const longLivedTokenResponse = await fetch('https://graph.instagram.com/access_token', {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: new URLSearchParams({
+        grant_type: 'ig_exchange_token',
+        client_secret: process.env.INSTAGRAM_APP_SECRET,
+        access_token: accessToken,
+      }),
+    });
+    
+    const longLivedTokenData = await longLivedTokenResponse.json();
+    
+    if (longLivedTokenData.error) {
+      return NextResponse.redirect(origin);
+    }
+
+    const { access_token, expires_at } = longLivedTokenData;
+
     // save the token to database
     // need to see about how i can automatically refresh the user's token (if possible)
     // redirect user to the next page
+    const forwardedHost = request.headers.get('x-forwarded-host'); // original origin before load balancer
+    const isLocalEnv = process.env.NODE_ENV === 'development';
+    if (isLocalEnv) {
+      // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+      return NextResponse.redirect(`${origin}${next}`);
+    } else if (forwardedHost) {
+      return NextResponse.redirect(`https://${forwardedHost}${next}`);
+    } else {
+      return NextResponse.redirect(`${origin}${next}`);
+    }
   }
 }
