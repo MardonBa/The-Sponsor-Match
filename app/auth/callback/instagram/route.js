d64fd31e14057660e6d1feb405fb84b80c4b91ee
Bayan Mardon <mardon.junior@gmail.com>
2025-01-07 21:22:35 -0500

    add some instagram logic

--- app/auth/callback/instagram/route.js
+++ app/auth/callback/instagram/route.js
@@ -13,7 +13,7 @@ export async function GET(request) {
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: new URLSearchParams({
         client_id: process.env.INSTAGRAM_APP_ID,
-        client_secret: process.envINSTAGRAM_APP_SECRET,
+        client_secret: process.env.INSTAGRAM_APP_SECRET,
         grant_type: 'authorization_code',
         redirect_uri: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback/instagram`,
         code,
@@ -26,11 +26,79 @@ export async function GET(request) {
       return NextResponse.redirect(origin);
     }
 
-    const { accessToken, userId } = tokenData;
+    const { accessToken } = tokenData;
+
+    const { longLivedAccessToken, expiresIn } = getLongLivedToken(accessToken);
+
+    // create a supabase instance
+    const supabase = await createClient();
     
-    // exchange the short lived token for a long lived one https://developers.facebook.com/docs/instagram-platform/instagram-api-with-instagram-login/business-login
-    // save the token to database
+    /*
+     save the token to database
+    */
+
+    // get the user id
+    const { data: { user } } = await supabase.auth.getUser()
+    const userId = user.id
+
+    // get the creator id based on the user id
+    let { data, error } = await supabase.rpc(
+      'get_creator_id', {
+        user_id: userId
+    });
+    if (error) {
+      console.error(error);
+      return NextResponse.redirect(origin);
+    }
+    const creatorId = data;
+
+    // get the platform id for instagram
+    ({ data, error } = await supabase.rpc(
+      'get_platform_id', {
+        name: 'instagram'
+    }));
+    if (error) {
+      console.error(error);
+      return NextResponse.redirect(origin);
+    }
+    const platformId = data;
+    // rpc call to update platforms_creators based on the creator_id
+    ({ data, error } = await supabase.rpc(
+      'update_platforms_creators', {
+        creator_id: creatorId, 
+        expires_in_seconds: expiresIn, 
+        new_access_token: longLivedAccessToken, 
+        new_refresh_token: null, 
+        platform_id: platformId
+    }));
+
+    if (error) {
+      console.error(error);
+      return NextResponse.redirect(origin);
+    }
+
     // need to see about how i can automatically refresh the user's token (if possible)
     // redirect user to the next page
+    else {
+      return NextResponse.redirect(next);
+    }
   }
 }
+
+const getLongLivedToken = async (shortLivedToken) => {
+  // exchange the short lived token for a long lived one https://developers.facebook.com/docs/instagram-platform/instagram-api-with-instagram-login/business-login\
+  const response = await fetch(
+    `https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=${process.env.INSTAGRAM_APP_SECRET}&access_token=${shortLivedToken}`
+  );
+
+  const data = await response.json();
+
+  if (data.error) {
+    return NextResponse.redirect(origin);
+  }
+
+  return {
+    longLivedAccessToken: data.access_token,
+    expiresIn: data.expires_in, // Token validity in seconds (60 days)
+  };
+};
--- app/ui/public/auth/action.js
+++ app/ui/public/auth/action.js
@@ -9,18 +9,10 @@ export async function oauthLogin(prov) {
 
   const supabase = await createClient();
 
-  let scopes;
-  if (prov == 'facebook') {
-    scopes = 'read_insights pages_read_engagement pages_show_list user_friends pages_read_user_content';
-  } else if (prov == 'google') {
-    scopes = '/auth/yt-analytics.readonly /auth/userinfo.email /auth/userinfo.profile openid'
-  }
-
   const { data, error } = await supabase.auth.signInWithOAuth({
       provider: prov,
       options: {
           redirectTo: 'http://localhost:3000/auth/callback',
-          scopes
       }
   });
 
@@ -31,7 +23,7 @@ export async function oauthLogin(prov) {
 
 // Supabase doesn't support instagram oauth, so I have to build it out myself. This function essentially does what supabase.auth.signInWithOAuth does
 export async function instagramOauthLogin(provider) { // Don't need to actually do anything with the parameter, it just needs to be there
-  const scopes = 'business_basic business_manage_messages business_manage_comments'
+  const scopes = 'instagram_business_basic instagram_business_manage_messages instagram_business_manage_comments'
   const redirectUri = encodeURIComponent(`${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback/instagram`);
   const authUrl = `https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=${process.env.INSTAGRAM_APP_ID}&redirect_uri=${redirectUri}&response_type=code&scope=${scopes}`;
 
