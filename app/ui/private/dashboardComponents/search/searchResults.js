e8aa163930d6aedeaf10de37417c66792ea39f6b
Bayan Mardon <mardon.junior@gmail.com>
2025-01-10 16:40:52 -0500

    plan next steps for search

--- app/ui/private/dashboardComponents/search/searchResults.js
+++ app/ui/private/dashboardComponents/search/searchResults.js
@@ -1,15 +1,29 @@
 import styles from './searchResults.module.css';
 import SearchResult from './searchResults/searchResult';
+import { getUserType } from '@/utils/helper/getUserType';
 
 export default function SearchResults() {
-    return (
-        <div className={styles.container} >
-            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
-            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
-            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
-            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
-            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
-            <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
-        </div>
-    );
+
+  // get the user type
+  const userType = getUserType();
+  // get a list of all the users of the opposite user type
+  let userData;
+  if (userType == "sponsor") {
+    // get the data of all sponsors
+  } else if (userType == "creator") {
+    // get the data of all creators
+  }
+  // look at what is retured to see if it already works
+  // make a list of dictionaries that are of the form {name, communitySize, niche, contentFrequency}
+
+  return (
+    <div className={styles.container} >
+      <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+      <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+      <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+      <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+      <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+      <SearchResult name={'Bayan'} communitySize={'13k'} niche={'games'} contentFrequency={'weekly'} />
+    </div>
+  );
 }
