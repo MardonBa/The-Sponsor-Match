4de004f8984e89681bedb3b0f190f220d7998351
Bayan Mardon <mardon.junior@gmail.com>
2025-01-20 09:02:26 -0500

    switch out the search container on the dedicated search page

--- app/(private)/search/page.js
+++ app/(private)/search/page.js
@@ -1,7 +1,7 @@
 import styles from './page.module.css';
 import Sidebar from '@/app/ui/private/dashboardComponents/sidebar/sidebar';
 import NavSideBar from '@/app/ui/private/dashboardComponents/sidebar/navsidebar';
-import SearchContainer from '@/app/ui/private/dashboardComponents/search/searchContainer';
+import SearchContainerWrapper from '@/app/ui/private/dashboardComponents/search/searchContainerWrapper';
 
 export default function SearchPage() {
     return (
@@ -9,7 +9,7 @@ export default function SearchPage() {
             <Sidebar style={styles.sidebar} >
                 <NavSideBar />
             </Sidebar>
-            <SearchContainer style={styles.search} />
+            <SearchContainerWrapper style={styles.search} />
         </div>
     );
 }
